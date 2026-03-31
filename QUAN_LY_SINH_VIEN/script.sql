cdf7f41fd62c9a83bf0a0e79b18af8f6eeac581a
Son Nguyen <sonbeo1907@gmail.com>
2026-03-31 15:08:58 +0700

    added quanlisinhvien

---
+++ QUAN_LY_SINH_VIEN/script.sql
@@ -0,0 +1,139 @@
+mysqldump: [Warning] Using a password on the command line interface can be insecure.
+-- MySQL dump 10.13  Distrib 9.2.0, for Linux (x86_64)
+--
+-- Host: localhost    Database: QuanLySinhVien
+-- ------------------------------------------------------
+-- Server version	9.2.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Class`
+--
+
+DROP TABLE IF EXISTS `Class`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Class` (
+  `ClassID` int NOT NULL AUTO_INCREMENT,
+  `ClassName` varchar(60) NOT NULL,
+  `StartDate` datetime NOT NULL,
+  `Status` bit(1) DEFAULT NULL,
+  PRIMARY KEY (`ClassID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Class`
+--
+
+LOCK TABLES `Class` WRITE;
+/*!40000 ALTER TABLE `Class` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Class` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Mark`
+--
+
+DROP TABLE IF EXISTS `Mark`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Mark` (
+  `MarkId` int NOT NULL AUTO_INCREMENT,
+  `SubId` int NOT NULL,
+  `StudentId` int NOT NULL,
+  `Mark` float DEFAULT '0',
+  `ExamTimes` tinyint DEFAULT '1',
+  PRIMARY KEY (`MarkId`),
+  UNIQUE KEY `SubId` (`SubId`,`StudentId`),
+  KEY `StudentId` (`StudentId`),
+  CONSTRAINT `Mark_ibfk_1` FOREIGN KEY (`SubId`) REFERENCES `Subject` (`SubId`),
+  CONSTRAINT `Mark_ibfk_2` FOREIGN KEY (`StudentId`) REFERENCES `Student` (`StudentId`),
+  CONSTRAINT `Mark_chk_1` CHECK ((`Mark` between 0 and 100))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Mark`
+--
+
+LOCK TABLES `Mark` WRITE;
+/*!40000 ALTER TABLE `Mark` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Mark` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Student`
+--
+
+DROP TABLE IF EXISTS `Student`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Student` (
+  `StudentId` int NOT NULL AUTO_INCREMENT,
+  `StudentName` varchar(30) NOT NULL,
+  `Address` varchar(50) DEFAULT NULL,
+  `Phone` varchar(20) DEFAULT NULL,
+  `Status` bit(1) DEFAULT NULL,
+  `ClassId` int NOT NULL,
+  PRIMARY KEY (`StudentId`),
+  KEY `ClassId` (`ClassId`),
+  CONSTRAINT `Student_ibfk_1` FOREIGN KEY (`ClassId`) REFERENCES `Class` (`ClassID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Student`
+--
+
+LOCK TABLES `Student` WRITE;
+/*!40000 ALTER TABLE `Student` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Student` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Subject`
+--
+
+DROP TABLE IF EXISTS `Subject`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Subject` (
+  `SubId` int NOT NULL AUTO_INCREMENT,
+  `SubName` varchar(30) NOT NULL,
+  `Credit` tinyint NOT NULL DEFAULT '1',
+  `Status` bit(1) DEFAULT b'1',
+  PRIMARY KEY (`SubId`),
+  CONSTRAINT `Subject_chk_1` CHECK ((`Credit` >= 1))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Subject`
+--
+
+LOCK TABLES `Subject` WRITE;
+/*!40000 ALTER TABLE `Subject` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Subject` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-03-31  8:08:44
